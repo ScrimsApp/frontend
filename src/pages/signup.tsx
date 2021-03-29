9639918ee5901494a0f5d2454b3d581046064213
savegnago <savegdesigner@gmail.com>
2021-03-29 14:55:07 -0300

    Adjust on SignUp Form :wrench:

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -33,10 +33,10 @@ const SignUp = () => {
 
   const formik = useFormik({
     initialValues: {
-      nickname: '',
+      name: '',
       email: '',
       password: '',
-      confirmPassword: '',
+      password_confirmation: '',
     },
     onSubmit: (values) => handleSignUp(values),
     validateOnChange: false,
@@ -45,10 +45,10 @@ const SignUp = () => {
 
   const handleSignUp = async (values: any) => {
     const response = await api.post<SignUpResponse>('/auth/register', {
-      name: values.nickname,
+      name: values.name,
       email: values.email,
       password: values.password,
-      password_confirmation: values.password,
+      password_confirmation: values.password_confirmation,
     });
 
     const { data, status } = response;
@@ -73,13 +73,13 @@ const SignUp = () => {
         </ErrorLabel>
 
         <SignInput
-          name="nickname"
+          name="name"
           minWidth="100%"
           colorType="secondary"
           label="Nickname"
           type="text"
           margin={['0px', '0px', '36px', '0px']}
-          value={formik.values.nickname}
+          value={formik.values.name}
           onChange={formik.handleChange}
         />
 
@@ -106,13 +106,13 @@ const SignUp = () => {
         />
 
         <SignInput
-          name="confirmPassword"
+          name="password_confirmation"
           minWidth="100%"
           colorType="secondary"
           label="Confirm Password"
           type="password"
           margin={['0px', '0px', '36px', '0px']}
-          value={formik.values.confirmPassword}
+          value={formik.values.password_confirmation}
           onChange={formik.handleChange}
         />
 
