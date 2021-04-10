6ca75fbde5a5388ad82d1bb930080158d52a06da
savegnago <savegdesigner@gmail.com>
2021-04-10 15:36:57 -0300

    Saving images on Firebase Storage :tada: :rocket:

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,9 +1,11 @@
 export const firebaseConfig = {
-  apiKey: process.env.FIREBASE_APIKEY,
-  authDomain: process.env.FIREBASE_AUTHDOMAIN,
-  projectId: process.env.FIREBASE_PROJECTID,
-  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
-  appId: process.env.FIREBASE_APPID,
-  measurementId: process.env.FIREBASE_MEASUREMENTID,
+  apiKey:
+    process.env.FIREBASE_APIKEY || 'AIzaSyCjYwB4LiWriEoV6AHRm8Sa9ET-8RpyBaM',
+  authDomain: process.env.FIREBASE_AUTHDOMAIN || 'scrimsapp.firebaseapp.com',
+  projectId: process.env.FIREBASE_PROJECTID || 'scrimsapp',
+  storageBucket: process.env.FIREBASE_STORAGEBUCKET || 'scrimsapp.appspot.com',
+  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID || '714173194628',
+  appId:
+    process.env.FIREBASE_APPID || '1:714173194628:web:1c0a6ca6d148e4c901fdd1',
+  measurementId: process.env.FIREBASE_MEASUREMENTID || 'G-WTFHXRTFDT',
 };
