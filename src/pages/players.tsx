09279d842a83df292f663ca7a81fb3b8f917868b
savegnago <savegdesigner@gmail.com>
2021-04-23 08:17:31 -0300

    Players page init :zap:

--- src/pages/players.tsx
+++ src/pages/players.tsx
@@ -1,5 +1,23 @@
+import Navbar from '../components/Navbar/Navbar.component';
+import { MainWrapper } from '../styles/shared/Wrapper/Wrapper.styles';
+
+import {
+  PlayersSectionTitle,
+  PlayersWrapper,
+} from '../styles/pages/players/players.styles';
+
 const Players = () => {
-  return <h1>hello world!</h1>;
+  return (
+    <MainWrapper>
+      <Navbar />
+
+      <PlayersSectionTitle>Players</PlayersSectionTitle>
+
+      <PlayersWrapper>
+        <h1>PlayersCardWrapper</h1>
+      </PlayersWrapper>
+    </MainWrapper>
+  );
 };
 
 export default Players;
---
+++ src/styles/pages/players/players.styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+import { TeamsWrapper, SectionTitle } from '../teams/teams.styles';
+
+export const PlayersWrapper = styled(TeamsWrapper)``;
+export const PlayersSectionTitle = styled(SectionTitle)``;
