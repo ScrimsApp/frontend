01559b915ac2d12bb8b3d0f8ceeebda3b405a3c1
savegnago <savegdesigner@gmail.com>
2021-04-28 08:55:00 -0300

    Index Matches :tada:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, useContext, useEffect, useState } from 'react';
+import { FunctionComponent } from 'react';
 import { GetServerSideProps } from 'next';
 
 import Navbar from '../components/Navbar/Navbar.component';
@@ -8,40 +8,28 @@ import {
   MatchesWrapper,
   SectionTitle,
 } from '../styles/pages/index/index.styles';
-import MatchCard from '../components/MatchCard/MatchCard.component';
 
 import { homeContent } from '../content/home/home.content';
 
-import Loading from '../components/Loading/Loading.component';
-import { GlobalContext } from '../context/GlobalContext.';
-
 import { api } from '../config/api';
 import { MatchesResponse } from '../types/responses/match/MatchesResponse.type';
+
+import MatchesCardWrapper from '../components/MatchesCardWrapper/MatchesCardWrapper.component';
 
 interface HomeProps {
   matches: Array<MatchesResponse>;
 }
 
 const Home: FunctionComponent<HomeProps> = ({ matches }) => {
-  const [isContentLoaded, setIsContentLoaded] = useState(false);
-  const { userContext } = useContext(GlobalContext);
-  const { user } = userContext;
-
-  console.log(matches);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setIsContentLoaded(true);
-    }, 1000);
-  });
-
   return (
     <MainWrapper>
       <Navbar />
 
       <SectionTitle>{homeContent.matches}</SectionTitle>
 
-      <MatchesWrapper>{/* Matches Cards goes here... */}</MatchesWrapper>
+      <MatchesWrapper>
+        <MatchesCardWrapper matches={matches} />
+      </MatchesWrapper>
     </MainWrapper>
   );
 };
