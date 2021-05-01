20dbe9e13fe036dd8abea685c0dfc859c8f59c15
savegnago <savegdesigner@gmail.com>
2021-05-01 15:38:07 -0300

    Adjust on Team Response Type :wrench:

--- src/types/responses/team/TeamResponse.type.ts
+++ src/types/responses/team/TeamResponse.type.ts
@@ -12,5 +12,24 @@ export interface TeamResponse {
   created_at: string;
   updated_at: string;
   players: Array<Player>;
-  invites: Array<Invite>;
+  invites_players: Array<Invite>;
+  invites_matches_receives: Array<any>;
+  matches_accepted: Array<any>;
+  invites_matches_sends: Array<MatchInviteSent>;
+  matches_created: Array<any>;
 }
+
+interface MatchInviteReceived {}
+
+interface MatchesScheduled {}
+
+interface MatchInviteSent {
+  created_at: string;
+  id: number;
+  match: any;
+  status: number;
+  team_2: number;
+  updated_at: string;
+}
+
+interface MatchCreated {}
