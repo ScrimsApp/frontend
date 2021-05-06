ae04caff319a11e1f5d523ed5c7f9cf00f124dd5
savegnago <savegdesigner@gmail.com>
2021-05-06 08:32:33 -0300

    Adding Context Provider to Test Utils :wrench:

--- src/__tests__/testUtils.tsx
+++ src/__tests__/testUtils.tsx
@@ -5,14 +5,24 @@ import '@testing-library/jest-dom';
 import { ThemeProvider } from 'styled-components';
 import theme from '../styles/theme/theme';
 
+import GlobalContextProvider from '../context/GlobalContext.';
+
 const AppThemeProvider: FC = ({ children }) => {
   return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+};
+
+const AppContextProvider: FC = ({ children }) => {
+  return (
+    <GlobalContextProvider>
+      <AppThemeProvider>{children}</AppThemeProvider>
+    </GlobalContextProvider>
+  );
 };
 
 const testRender = (
   ui: ReactElement,
   options?: Omit<RenderOptions, 'queries'>
-) => render(ui, { wrapper: AppThemeProvider, ...options });
+) => render(ui, { wrapper: AppContextProvider, ...options });
 
 export * from '@testing-library/react';
 
