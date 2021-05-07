0aa8dfa4b5ee06d89e007cb4c3d42b06b2581fd2
savegnago <savegdesigner@gmail.com>
2021-05-07 08:30:41 -0300

    Adjust on Player type :wrench:

--- src/types/player/Player.type.ts
+++ src/types/player/Player.type.ts
@@ -1,11 +1,8 @@
 export interface Player {
   id: number;
   name: string;
-  email: string;
-  email_verified_at: any;
-  person_id: any;
   image: any;
-  created_at: string;
-  updated_at: string;
+  person_id: any;
   team_id: number;
+  created_at: string;
 }
