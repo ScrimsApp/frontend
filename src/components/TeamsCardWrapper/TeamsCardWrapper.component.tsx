c9a43d54d485c37092addb33f8e763fc3dc65ecd
savegnago <savegdesigner@gmail.com>
2021-05-10 09:09:15 -0300

    Adding useScrollFetch hook to Teams page :zap:

--- src/components/TeamsCardWrapper/TeamsCardWrapper.component.tsx
+++ src/components/TeamsCardWrapper/TeamsCardWrapper.component.tsx
@@ -1,86 +1,26 @@
-import {
-  FunctionComponent,
-  useContext,
-  useEffect,
-  useRef,
-  useState,
-} from 'react';
-import useSWR from 'swr';
+import { FunctionComponent } from 'react';
 
 import { TeamsCardWrapperProps } from './types';
 
-import { api } from '../../config/api';
-import { TeamsResponse } from '../../types/responses/team/TeamsResponse.type';
-
 import TeamsCard from '../TeamsCard/TeamsCard.component';
 import Loading from '../Loading/Loading.component';
-import { GlobalContext } from '../../context/GlobalContext.';
+
+import useScrollFetch from '../../hooks/useScrollFetch';
 
 const TeamsCardWrapper: FunctionComponent<TeamsCardWrapperProps> = ({
   teams,
 }) => {
-  const { notificationContext } = useContext(GlobalContext);
-  const { setNotificationStatus, setNewNotification } = notificationContext;
-  const [allTeams, setAllTeams] = useState(teams.data);
-  const [pageNumber, setPageNumber] = useState(1);
-
-  const observerRef = useRef(null);
-
-  const fetcher = (url: string) => api.get(url).then((res) => res.data);
-
-  const { data, error, isValidating, mutate } = useSWR<TeamsResponse>(
-    `teams?page=${pageNumber}`,
-    fetcher,
-    {
-      initialData: teams,
-    }
+  const { allData, observerRef, isLoading } = useScrollFetch(
+    1,
+    teams.last_page,
+    teams.total,
+    teams.data
   );
 
-  useEffect(() => {
-    if (pageNumber === teams.last_page) return;
-
-    const intersectionObserver = new IntersectionObserver(
-      async (entries: Array<IntersectionObserverEntry>) => {
-        const ratio = entries[0].intersectionRatio;
-
-        if (!isValidating && ratio > 0.6 && allTeams.length < teams.total) {
-          setPageNumber((prevPage) => {
-            if (prevPage < teams.last_page) {
-              return prevPage + 1;
-            } else {
-              return prevPage;
-            }
-          });
-
-          const { data } = await mutate();
-          setAllTeams((prevTeams) => [...new Set([...prevTeams, ...data])]);
-        }
-      },
-      {
-        threshold: 0.6,
-      }
-    );
-
-    if (observerRef.current) intersectionObserver.observe(observerRef.current);
-
-    return () => {
-      intersectionObserver.disconnect();
-    };
-  }, [allTeams]);
-
-  if (error) {
-    setNotificationStatus(true);
-    setNewNotification({
-      type: 'error',
-      title: 'Error',
-      message: error,
-    });
-  }
-
-  if (allTeams?.length > 0) {
+  if (allData?.length > 0) {
     return (
       <>
-        {allTeams.map((team) => (
+        {allData.map((team) => (
           <TeamsCard
             id={team.id}
             teamImage={`http://localhost:8000/storage/${team.image}`}
@@ -96,7 +36,7 @@ const TeamsCardWrapper: FunctionComponent<TeamsCardWrapperProps> = ({
     );
   }
 
-  if (isValidating) {
+  if (isLoading) {
     return <Loading />;
   }
 
