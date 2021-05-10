4b7175f6f4cbc701ee63e1e91033c2f18619595e
savegnago <savegdesigner@gmail.com>
2021-05-10 08:26:22 -0300

    Adding pagination + Fetch data on scroll :tada:

--- src/components/TeamsCardWrapper/TeamsCardWrapper.component.tsx
+++ src/components/TeamsCardWrapper/TeamsCardWrapper.component.tsx
@@ -1,4 +1,10 @@
-import { FunctionComponent, useContext } from 'react';
+import {
+  FunctionComponent,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import useSWR from 'swr';
 
 import { TeamsCardWrapperProps } from './types';
@@ -15,16 +21,52 @@ const TeamsCardWrapper: FunctionComponent<TeamsCardWrapperProps> = ({
 }) => {
   const { notificationContext } = useContext(GlobalContext);
   const { setNotificationStatus, setNewNotification } = notificationContext;
+  const [allTeams, setAllTeams] = useState(teams.data);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const observerRef = useRef(null);
 
   const fetcher = (url: string) => api.get(url).then((res) => res.data);
 
-  const { data, error, isValidating } = useSWR<TeamsResponse>(
-    `teams`,
+  const { data, error, isValidating, mutate } = useSWR<TeamsResponse>(
+    `teams?page=${pageNumber}`,
     fetcher,
     {
       initialData: teams,
     }
   );
+
+  useEffect(() => {
+    if (pageNumber === teams.last_page) return;
+
+    const intersectionObserver = new IntersectionObserver(
+      async (entries: Array<IntersectionObserverEntry>) => {
+        const ratio = entries[0].intersectionRatio;
+
+        if (!isValidating && ratio > 0.6 && allTeams.length < teams.total) {
+          setPageNumber((prevPage) => {
+            if (prevPage < teams.last_page) {
+              return prevPage + 1;
+            } else {
+              return prevPage;
+            }
+          });
+
+          const { data } = await mutate();
+          setAllTeams((prevTeams) => [...new Set([...prevTeams, ...data])]);
+        }
+      },
+      {
+        threshold: 0.6,
+      }
+    );
+
+    if (observerRef.current) intersectionObserver.observe(observerRef.current);
+
+    return () => {
+      intersectionObserver.disconnect();
+    };
+  }, [allTeams]);
 
   if (error) {
     setNotificationStatus(true);
@@ -35,10 +77,10 @@ const TeamsCardWrapper: FunctionComponent<TeamsCardWrapperProps> = ({
     });
   }
 
-  if (data?.data?.length > 0) {
+  if (allTeams?.length > 0) {
     return (
       <>
-        {data.data.map((team) => (
+        {allTeams.map((team) => (
           <TeamsCard
             id={team.id}
             teamImage={`http://localhost:8000/storage/${team.image}`}
@@ -49,6 +91,7 @@ const TeamsCardWrapper: FunctionComponent<TeamsCardWrapperProps> = ({
             key={team.name}
           />
         ))}
+        <div ref={observerRef} />
       </>
     );
   }
@@ -57,7 +100,7 @@ const TeamsCardWrapper: FunctionComponent<TeamsCardWrapperProps> = ({
     return <Loading />;
   }
 
-  return <p>There are no teams yet :(</p>;
+  return <p>There are no registered teams yet :(</p>;
 };
 
 export default TeamsCardWrapper;
