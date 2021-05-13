671a969e83059b7e157aed46ff79a07a8a9ea807
savegnago <savegdesigner@gmail.com>
2021-05-13 08:49:04 -0300

    Adding babel plugin inline react svg :package:

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    "presets": ["next/babel", "@babel/preset-typescript"],
-    "plugins": [["styled-components", { "ssr": true }]]
-}
+  presets: ['next/babel', '@babel/preset-typescript'],
+  plugins: [['styled-components', { ssr: true }], 'inline-react-svg'],
+};
