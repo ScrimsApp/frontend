b365f7b85b96a01a67c66a9537c91adb75a502c7
savegnago <savegdesigner@gmail.com>
2021-05-18 08:05:35 -0300

    Adding Player by id  info page :tada:

---
+++ src/styles/pages/player/playerById.styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+import { TeamByIdWrapper, TeamByIdInfoWrapper } from '../team/teamById.styles';
+
+export const PlayerByIdWrapper = styled(TeamByIdWrapper)``;
+export const PlayerByIdInfoWrapper = styled(TeamByIdInfoWrapper)``;
