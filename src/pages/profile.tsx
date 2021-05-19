b78a5d4770d5f1a37b3994b9ca793588e45716d0
savegnago <savegdesigner@gmail.com>
2021-05-18 21:29:18 -0300

    Adding the Player Profile Wrapper component to Profile page :zap: :wrench:

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,10 +1,4 @@
-import {
-  ChangeEvent,
-  FunctionComponent,
-  useContext,
-  useEffect,
-  useState,
-} from 'react';
+import { FunctionComponent, useContext, useEffect, useState } from 'react';
 
 import { MainWrapper } from '../styles/shared/Wrapper/Wrapper.styles';
 import Navbar from '../components/Navbar/Navbar.component';
@@ -16,20 +10,10 @@ import { Player } from '../types/player/Player.type';
 
 import { api } from '../config/api';
 
-import {
-  ProfileWrapper,
-  PlayerWrapper,
-  Image,
-  Form,
-} from '../styles/pages/profile/profile.styles';
+import { ProfileWrapper } from '../styles/pages/profile/profile.styles';
 
-import SignInput from '../components/SignInput/SignInput.component';
-import {
-  Button,
-  ButtonOverlay,
-  ButtonWrapper,
-} from '../styles/shared/Button/Button.styles';
 import Loading from '../components/Loading/Loading.component';
+import PlayerProfileWrapper from '../components/PlayerProfileWrapper/PlayerProfileWrapper.component';
 
 const Profile: FunctionComponent = () => {
   const { userContext, notificationContext } = useContext(GlobalContext);
@@ -44,27 +28,29 @@ const Profile: FunctionComponent = () => {
   } as Player);
 
   useEffect(() => {
-    if (!user.token) router.back();
-
-    api
-      .get('player', {
-        headers: {
-          Authorization: `Bearer ${user.token}`,
-        },
-      })
-      .then((res) => {
-        setPlayer(res.data);
-        setIsloading(false);
-      })
-      .catch((error) => {
-        setNotificationStatus(true);
-        setNewNotification({
-          type: 'error',
-          title: 'Whoops!',
-          message: error.message,
+    if (!user.token) {
+      router.back();
+    } else {
+      api
+        .get('player', {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        })
+        .then((res) => {
+          setPlayer(res.data);
+          setIsloading(false);
+        })
+        .catch((error) => {
+          setNotificationStatus(true);
+          setNewNotification({
+            type: 'error',
+            title: 'Whoops!',
+            message: error.message,
+          });
         });
-      });
-  }, []);
+    }
+  }, [user]);
 
   return (
     <MainWrapper>
@@ -72,50 +58,7 @@ const Profile: FunctionComponent = () => {
 
       {!isLoading ? (
         <ProfileWrapper>
-          <PlayerWrapper>
-            <Image
-              src={`https://avatars.dicebear.com/api/micah/${player.image}`}
-              alt={player.name}
-            />
-
-            <Form>
-              <SignInput
-                name="name"
-                minWidth="100%"
-                colorType="primary"
-                label="Name"
-                type="text"
-                margin={['0px', '0px', '36px', '0px']}
-                value={player.name}
-                onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                  setPlayer({ ...player, name: e.target.value })
-                }
-                initialValue={'player'}
-              />
-
-              <SignInput
-                name="email"
-                minWidth="100%"
-                colorType="primary"
-                label="E-mail"
-                type="email"
-                margin={['0px', '0px', '36px', '0px']}
-                value={player.email}
-                onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                  setPlayer({ ...player, email: e.target.value })
-                }
-                initialValue={'player'}
-              />
-
-              <ButtonWrapper
-                minWidth="100%"
-                margin={['0px', '0px', '36px', '0px']}
-              >
-                <ButtonOverlay className="overlay" type="secondary" />
-                <Button type="submit">Update</Button>
-              </ButtonWrapper>
-            </Form>
-          </PlayerWrapper>
+          <PlayerProfileWrapper initialPlayer={player} />
         </ProfileWrapper>
       ) : (
         <Loading fullPage />
--- src/styles/pages/profile/profile.styles.ts
+++ src/styles/pages/profile/profile.styles.ts
@@ -16,34 +16,6 @@ export const ProfileWrapper = styled.section`
   }
 `;
 
-export const PlayerWrapper = styled.div`
-  width: 50%;
-
-  display: flex;
-  flex-direction: column;
-
-  box-sizing: border-box;
-  background-color: #262a3b;
-
-  @media only screen and (max-width: 840px) {
-    width: 100%;
-  }
-`;
-
-export const Image = styled.img`
-  width: 100%;
-  max-height: 350px;
-
-  object-fit: cover;
-`;
-
-export const Form = styled.form`
-  width: 100%;
-
-  box-sizing: border-box;
-  padding: 36px 36px 0px 36px;
-`;
-
 export const TeamWrapper = styled.div`
   width: 50%;
 `;
