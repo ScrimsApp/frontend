3a60bb1d67e6b642943021f917600726a51830e6
savegnago <savegdesigner@gmail.com>
2021-06-06 12:08:00 -0300

    Adjusting Create Match Time format RegExp :wrench:

--- src/utils/validation/match/createMatch.schema.ts
+++ src/utils/validation/match/createMatch.schema.ts
@@ -4,12 +4,12 @@ export const createMatchSchema = Yup.object().shape({
   type: Yup.string().required(),
   date: Yup.string()
     .matches(/^(0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/, {
-      message: 'Date format should be as: 04/25',
+      message: 'Date format should be (month/day)',
     })
     .required(), // 04/25
   time: Yup.string()
-    .matches(/^([0-9]|[0-2][0-4])[\:]([0-5][0-9])$/, {
-      message: 'Time format should be as: 9:00',
+    .matches(/^([0-9]|[0-2][0-4]|[0-1][0-9])[\:]([0-5][0-9])$/, {
+      message: 'Time format should be (hour:minutes)',
     })
     .required(), // 08:00
 });
