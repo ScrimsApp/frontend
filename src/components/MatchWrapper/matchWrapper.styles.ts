864d2a76163001dc8feb90842afe51af4d95c87d
savegnago <savegdesigner@gmail.com>
2021-06-08 15:18:58 -0300

    Adding RoomInfo to Match page :wrench: :zap:

--- src/components/MatchWrapper/matchWrapper.styles.ts
+++ src/components/MatchWrapper/matchWrapper.styles.ts
@@ -15,3 +15,41 @@ export const MatchMainWrapper = styled.section`
     flex-direction: column;
   }
 `;
+
+export const RoomInfo = styled.div`
+  width: 100%;
+  height: auto;
+  position: relative;
+
+  padding: 36px;
+  box-sizing: border-box;
+
+  border-radius: 12px;
+  background-color: #1f2333;
+`;
+
+export const RoomInfoTitle = styled.h2`
+  width: 80%;
+  text-align: left;
+  font-size: 24px;
+  font-weight: 600;
+  color: ${(props) => props.theme.colors.text.light};
+
+  margin-bottom: 20px;
+`;
+
+export const RoomInfoText = styled.p`
+  width: 100%;
+  text-align: left;
+  font-size: 16px;
+  font-weight: 300;
+  color: ${(props) => props.theme.colors.text.light};
+
+  &&:not(:last-child) {
+    margin-bottom: 15px;
+  }
+`;
+
+export const HightlightSpan = styled.span`
+  color: ${(props) => props.theme.colors.secondary};
+`;
